d929168ce30b8ad56bea5da0ae4c74396646b33e
Cuchillon <ferickadmin@gmail.com>
2019-08-22 17:54:43 +0300

    Adding request verifier javadoc

--- standalone/src/main/java/com/ferick/alexander/utils/RequestVerifier.java
+++ standalone/src/main/java/com/ferick/alexander/utils/RequestVerifier.java
@@ -10,6 +10,18 @@ import spark.utils.SparkUtils;
 
 public class RequestVerifier {
 
+    /**
+     * Method compares paths considering templates which contain named parameters (:name) or wildcards (*)
+     * Examples:
+     * '/some/path' equals '/some/path'
+     * '/some/path' equals '/some/*'
+     * '/some/:name' equals '/some/path'
+     * Part count separated by '/' must be equal
+     *
+     * @param requestPath path from received request
+     * @param contractPath path from contract stored in {@link com.ferick.alexander.storage.ContractStorage}
+     * @return true if paths are equal, otherwise false
+     */
     public static boolean matchPaths(String requestPath, String contractPath) {
 
         if (requestPath.equals(contractPath)) {
@@ -45,25 +57,38 @@ public class RequestVerifier {
         return false;
     }
 
+    /**
+     * Method compares received request with request template contained in stored contract
+     *
+     * @param request received request
+     * @param contract contract stored in {@link com.ferick.alexander.storage.ContractStorage}
+     * @return true if request is matched, otherwise false
+     */
     public static boolean matchRequest(Request request, Contract contract) {
         boolean isMatched = true;
         RequestDTO contractRequest = contract.getRequest();
-        if (contractRequest.getMethod().equals(request.requestMethod())) {
-            String requestBody = StringUtils.removeSpecialSymbols(request.body());
+        String requestBody = StringUtils.removeSpecialSymbols(request.body());
 
-            if (!requestBody.equals(contractRequest.getBody())) {
+        if (!requestBody.equals(contractRequest.getBody())) {
+            isMatched = false;
+        }
+        for (Map.Entry<String, String> entry : contractRequest.getHeaders().entrySet()) {
+            String headerValue = request.headers(entry.getKey());
+            if (headerValue == null || !headerValue.equals(entry.getValue())) {
                 isMatched = false;
             }
-            for (Map.Entry<String, String> entry : contractRequest.getHeaders().entrySet()) {
-                String headerValue = request.headers(entry.getKey());
-                if (headerValue == null || !headerValue.equals(entry.getValue())) {
-                    isMatched = false;
-                }
-            }
         }
+
         return isMatched;
     }
 
+    /**
+     * Method extracts named parameters, if present, from path
+     *
+     * @param requestPath path from received request
+     * @param contractPath path from contract stored in {@link com.ferick.alexander.storage.ContractStorage}
+     * @return map where name of parameter is key
+     */
     public static Map<String, String> getPathParams(String requestPath, String contractPath) {
         Map<String, String> pathParams = new HashMap<>();
         List<String> requestPathList = SparkUtils.convertRouteToList(requestPath);
@@ -80,6 +105,14 @@ public class RequestVerifier {
         return pathParams;
     }
 
+    /**
+     * Method puts values of named parameters into {{:name}} blocks in stored request template body,
+     * if the body contains such blocks and block name is the same as parameter name
+     *
+     * @param pathParams parameters map
+     * @param bodyTemplate body from request template in stored contract
+     * @return
+     */
     public static String checkPathParams(Map<String, String> pathParams, String bodyTemplate) {
         String responseBody = bodyTemplate.replaceAll("\\s+", "");
         for (Map.Entry<String, String> entry : pathParams.entrySet()) {
